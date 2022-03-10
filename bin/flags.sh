6b647e406eceb53752a806ad4ed82fe4b080983f
Alissa Huskey <alissa.huskey@gmail.com>
2022-03-10 00:19:13 -0700

    add PKG_CONFIG_PATH to flags.sh

--- bin/flags.sh
+++ bin/flags.sh
@@ -15,43 +15,52 @@ shopt -s globstar
 # usage: find_dirs PATH PATTERN
 #
 find_dirs() {
-  local path="$1" pattern="$2"
+  local dirpath="$1" pattern="$2"
+  local -a matches
 
-  printf "%s\n" "${path}/"**/*.${pattern} | sed -Ee "s_/[^/]*\.${pattern}\$__" | sort -u
+  # recursively find files ending with pattern
+  matches=( "${dirpath}/"**/*.${pattern} )
+
+  # return if there are none
+  [[ ${#matches[@]} -gt 0 ]] || return
+
+  # print a unique list of directories that the files are in
+  printf "%s\n" "${matches[@]}" | sed -Ee "s_/[^/]*\.${pattern}\$__" | sort -u
 }
 
-# generate shell commands to set CFLAGS and LDFLAGS environment variables
+# generate shell commands to set CFLAGS, LDFLAGS, and PKG_CONFIG_PATH
+# environment variables
 #
 # usage: make_flags
 #
 make_flags() {
 
-  local -a includes libs
-  local prefix pkg
+  local -a includes libs cfgs
+  local prefix cfgs
 
   includes+=( $(xcrun --show-sdk-path)/usr/include )
 
   for pkg in openssl readline bzip2 xz; do
     prefix=$(brew --prefix $pkg)
 
-    if [[ -d ${prefix}/include ]]; then
+    # find all directories with .h files
+    [[ -d ${prefix}/include ]] && includes+=( $(find_dirs "${prefix}/include" "h") )
 
-      # find all directories with .h files
-      includes+=( $(find_dirs "${prefix}/include" "h") )
-    fi
+    # find all directories with .a files
+    [[ -d ${prefix}/lib ]] && libs+=( $(find_dirs "${prefix}/lib" "a") )
 
-    if [[ -d ${prefix}/lib ]]; then
-
-      # find all directories with .a files
-      libs+=( $(find_dirs "${prefix}/lib" "a") )
-    fi
+    # find all directories with .pc files
+    [[ -d ${prefix}/lib/pkgconfig ]] && cfgs+=( $(find_dirs "${prefix}/lib/pkgconfig" "pc") )
 
   done
 
   printf -- 'CFLAGS="-I %s $CFLAGS"\n' "${includes[@]}"
   printf -- 'LDFLAGS="-L %s $LDFLAGS"\n' "${libs[@]}"
+  printf -- 'PKG_CONFIG_PATH="%s:${PKG_CONFIG_PATH}"\n' "${cfgs[@]}"
 
-  printf "\n\nexport CFLAGS LDFLAGS\n"
+  printf -- 'PYTHON_CONFIGURE_OPTS="--enable-shared"\n'
+
+  printf "\nexport CFLAGS LDFLAGS PKG_CONFIG_PATH PYTHON_CONFIGURE_OPTS\n"
 
 }
 
